d5a6d2de20db5d7bfde2dce34e78a9e2f0d30299
Gar Adamyan <ara50@gmail.com>
2021-07-01 23:20:58 +0400

    class

---
+++ script.js
@@ -0,0 +1,37 @@
+// HomeWork N1;
+// function addUp(num) {
+//     let tiv = 4;
+// 	for(let i = 1; i <= tiv; i++) {
+//         a.push(num = i);
+//         result = result + a.length;
+//     }
+// }
+// let result = 0;
+// let a = [];
+// addUp(4);
+// console.log(result);
+
+// HomeWork N3;
+// let result = 0;
+// function getAbsSum(arr) {
+// 	for(let i = 0; i < arr.length; i++) {
+//         if(arr[i] < 0) {
+//             result = result + (arr[i] * -1); 
+//             continue;   
+//         }
+//         result = result + arr[i];
+//     }
+//     return Math.abs(result);
+// }
+// getAbsSum([2, -1, 4, 8, 10]);
+// console.log(result);
+
+// HomeWork N4
+// function mean(arr) {
+// 	for(let i = 0; i < arr.length; i++) {
+//         result = result + arr[i]; 
+//     }
+//     return result / arr.length;
+// }
+// let result = 0;
+// console.log(mean([1, 0, 4, 5, 2, 4, 1, 2, 3, 3, 3]));
